f671d89eeed4d44415fa4c2b9a63b1ebcd6a6872
jonathan-briere <jonathan.briere.dev@gmail.com>
2023-07-04 09:37:28 -0400

    Configured memory store as cache store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,19 +18,12 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
+  config.cache_store = :memory_store
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
   end
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -45,12 +38,6 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
